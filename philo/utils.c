875a224a2f155073f3db41534e8610f3abc9845b
adil-mabrouk <adilemabrouk@gmail.com>
2024-09-09 22:56:30 +0100

    sabr

---
+++ philo/utils.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amabrouk <amabrouk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 12:25:27 by amabrouk          #+#    #+#             */
+/*   Updated: 2024/09/09 17:39:26 by amabrouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_usleep(size_t milliseconds, t_args *args)
+{
+	size_t	start;
+
+	start = get_time();
+	while ((get_time() - start) < milliseconds)
+	{
+		pthread_mutex_lock(&args->dead_mutex);
+		if (args->dead == 1)
+			return (pthread_mutex_unlock(&args->dead_mutex), 0);
+		pthread_mutex_unlock(&args->dead_mutex);
+		usleep(500);
+	}
+	return (0);
+}
+
+size_t	get_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		printf("gettimeofday() error\n");
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int	error(char *str)
+{
+	printf("%s\n", str);
+	return (1);
+}
